50c669c83554b9c4afb2780175d14cbd6c6727fa
arslan039 <105734610+arslan039@users.noreply.github.com>
2023-11-21 09:07:24 -0500

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -9,11 +9,12 @@ export default function Alert(props) {
   return (
     <>
       <div style={{ height: "50px" }}>
-        {props.alert && (
-          <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-            <strong>{Capitalize(props.alert.type)}</strong> {props.alert.message}
-          </div>
-        )}
+       {props.alert && props.alert.type && (
+  <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+    <strong>{Capitalize(props.alert.type)}</strong> {props.alert.message}
+  </div>
+)}
+
       </div>
     </>
   );
